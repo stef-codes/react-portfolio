76bb19183d7d6eedc9b4691c23a8c39a925e8db2
Stefanie <stefcodes@gmail.com>
2020-04-03 23:26:27 -0700

    Change header text and add email

--- src/yourdata.js
+++ src/yourdata.js
@@ -4,17 +4,17 @@ export default
         //Change The Website Template
         name :'stef.',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
+                        'Hi 👋! I am Stefanie Davis',
                         //Line 2 For Header
-                        'Digital designer ',
+                        'Software Engineer | Problem Solver ',
                         //Line 3 For Header
-                        'from India'
+                        'from Los Angeles'
     ],
         //Contact Email
-        contactEmail:'hello@chetanverma.com',
+        contactEmail:'stefcodes@gmail.com',
         // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        abouttext: "Former Data Consultant turned Full Stack Software Engineer. Passion for music and building with technology.",
+        aboutImage:'',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
