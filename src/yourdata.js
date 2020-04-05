2ed5556fe9c3203e0c39beb0607036ab4cf310c6
Stefanie <stefcodes@gmail.com>
2020-04-04 22:43:47 -0700

    update links and add first project

--- src/yourdata.js
+++ src/yourdata.js
@@ -21,33 +21,33 @@ export default
        projects:[
            {
             id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Project One', //Project Title - Add Your Project Title Here
-             service:'UI/UX Design', // Add Your Service Type Here
+            title:'Music Journal', //Project Title - Add Your Project Title Here
+             service:'React | Rails', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
              imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
              //Project URL - Add Your Project Url Here
-             url:'http://chetanverma.com/'
+             url:'https://github.com/stef-codes'
             },
             {
                 id: 2,//DO NOT CHANGE THIS (Please)😅
                 title: 'Project Two',
                 service: 'Website Development',
                 imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'https://github.com/stef-codes'
             },
             { 
                 id: 3,//DO NOT CHANGE THIS (Please)😅
                 title: 'Project Three',
                 service: 'Web App',
                 imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'https://github.com/stef-codes'
             },
             {
                 id: 4,//DO NOT CHANGE THIS (Please)😅
                 title: 'Project Four',
                 service: 'Branding',
                 imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'https://github.com/stef-codes'
            }
 
                     /*
