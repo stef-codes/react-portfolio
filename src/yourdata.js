cdd86824640268e03fbddf89019e8e0e742500b2
Stefanie <stefcodes@gmail.com>
2020-05-18 18:31:20 -0700

    add tech stack for React project

--- src/yourdata.js
+++ src/yourdata.js
@@ -22,7 +22,7 @@ export default
            {
             id: 1,//DO NOT CHANGE THIS (Please)😅
             title:'Music Journal', //Project Title - Add Your Project Title Here
-             service:'React | Rails', // Add Your Service Type Here
+             service:'React | Redux | iTunes API | Rails | Postgres', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
              imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
              //Project URL - Add Your Project Url Here
